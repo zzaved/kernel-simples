d0f47fd1c1df79d66881e9dc6756d4808bb13b64
Pablo Zaved <pablo.azevedo@sou.inteli.edu.br>
2025-09-23 03:45:37 -0300

    Create kernel.asm

---
+++ kernel.asm
@@ -0,0 +1,48 @@
+;;kernel.asm
+bits 32
+section .text
+        ;multiboot spec
+        align 4
+        dd 0x1BADB002            ;magic
+        dd 0x00                  ;flags
+        dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
+
+global start
+global keyboard_handler
+global read_port
+global write_port
+global load_idt
+
+extern kmain
+extern keyboard_handler_main
+
+read_port:
+	mov edx, [esp + 4]
+	in al, dx
+	ret
+
+write_port:
+	mov   edx, [esp + 4]
+	mov   al, [esp + 4 + 4]
+	out   dx, al
+	ret
+
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti
+	ret
+
+keyboard_handler:
+	call keyboard_handler_main
+	iretd
+
+start:
+  cli 			;clear interrupts
+  mov esp, stack_space	;set stack pointer
+  call kmain		;call main function
+  hlt		 	;halt the CPU
+
+section .bss
+resb 8192		;8KB for stack
+stack_space:
